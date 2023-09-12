13b34472eb05d33b7d4dce6628d7d7236f20b928
JoaoEnrique13 <joao.alves254@etec.sp.gov.br>
2023-09-12 20:47:42 -0300

    Logcat e imagem com Compose

---
+++ app/src/main/java/com/example/appcompose/MainActivity.kt
@@ -0,0 +1,163 @@
+package com.example.appcompose
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.appcompose.ui.theme.AppComposeTheme
+import com.example.appcompose.ui.theme.DebugButtonColors
+import com.example.appcompose.ui.theme.ErrorButtonColors
+import com.example.appcompose.ui.theme.InfoButtonColors
+import com.example.appcompose.ui.theme.WarningButtonColors
+
+const val TAG = "TestAndroid"
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppComposeTheme {
+                // A surface container using the 'background' color from the theme
+                App()
+            }
+        }
+    }
+}
+
+@Composable
+private fun App() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ){
+        Column(
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Greeting("Jetpack")
+
+            Row(){
+                Image(
+                    painter = painterResource(id = R.drawable.logo),
+                    contentDescription = "My test",
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clip(CircleShape)
+                        .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+                )
+            }
+
+
+            ActionButton(
+                text = "Debug",
+                buttonColors = DebugButtonColors(),
+                modifier = Modifier.fillMaxWidth(0.5f)
+            ){
+                Log.d(TAG, "App: Clicou em DEBUG!")
+            }
+            ActionButton(
+                text = "Info",
+                buttonColors = InfoButtonColors(),
+                modifier = Modifier.fillMaxWidth(0.5f)
+            ){
+                Log.i(TAG, "App: Clicou em INFO!")
+            }
+            ActionButton(
+                text = "Warning",
+                buttonColors = WarningButtonColors(),
+                modifier = Modifier.fillMaxWidth(0.5f)
+            ){
+                Log.w(TAG, "App: Clicou em WARNING!")
+            }
+            ActionButton(
+                text = "Error",
+                buttonColors = ErrorButtonColors(),
+                modifier = Modifier.fillMaxWidth(0.5f)
+            ){
+                try {
+                    throw RuntimeException("Clicou em ERROR!")
+                }catch (ex: Exception){
+                    Log.e(TAG, "${ex.message}")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ActionButton(
+    text: String,
+    buttonColors: ButtonColors = ButtonDefaults.buttonColors(),
+    modifier: Modifier = Modifier,
+    block: () -> Unit
+){
+    ElevatedButton(
+        onClick = block,
+        shape = RoundedCornerShape(5.dp),
+        colors = buttonColors,
+        modifier = modifier
+    ) {
+        Text(text = text)
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier){
+    Text(
+        text = "Aula de $name!",
+        style = MaterialTheme.typography.bodyLarge.copy(
+            fontWeight = FontWeight.Bold
+        ),
+        color = MaterialTheme.colorScheme.secondary
+    )
+}
+
+@Preview(showBackground = true, widthDp = 150, heightDp = 200)
+@Composable
+fun AppPreview(){
+    AppComposeTheme {
+        App()
+    }
+}
+
+@Preview(showBackground = true, widthDp = 120)
+@Composable
+fun ActionButtonPreview(){
+    ActionButton(text = "Cadastrar") {
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview(){
+    AppComposeTheme {
+        Greeting("Jetpack")
+    }
+}
